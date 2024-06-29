87f222fc2c87007b2ddaa79f7fb9b727d96541b5
mohamed ahmed saker <mohamedahmedsaker307@gmail.com>
2024-06-29 12:03:10 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-"# generative-AI-app" 
+it is a text-image generation using stable difussion
+***
+### requirements 
+- python 3.8 or latter
+- install python from pthon page [https://www.python.org/downloads/]
+***
+#### installations
+- cd src
+
+- pip install -r requirements.txt
+***
+
---
+++ src/model.py
@@ -0,0 +1,20 @@
+from diffusers import DiffusionPipeline
+import torch
+from PIL import Image
+from typing import Any
+
+def load_model():
+    model = DiffusionPipeline.from_pretrained(
+        "stabilityai/stable-diffusion-xl-base-1.0",
+        torch_dtype=torch.float16,
+        use_safetensors=True,
+        variant="fp16"
+    )
+    #model.to("cpu")
+    model.enable_model_cpu_offload()
+    return model
+
+def generate_image(model: Any, text: str) -> Image:
+    result = model(prompt=text)
+    image = result.images[0]
+    return image
